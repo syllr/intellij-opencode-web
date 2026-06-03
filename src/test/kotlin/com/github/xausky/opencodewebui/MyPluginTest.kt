a0159de4d8b2696dc8b9ec9bdf33938b10f7f64f
shenyuanlaolarou <shenyuanlaolarou@gmail.com>
2026-06-03 09:36:43 +0800

    chore: 清理废弃调研文档和测试数据，更新 .gitignore
    - 删除 research/idea-plugin-integration/ 下 13 份调研文档
    - 删除 research/performance/ 下 3 份性能分析文档
    - 删除 src/test/testData/rename/ 下测试数据
    - 简化 MyPluginTest.kt，移除不相关测试
    - .gitignore 添加 .omo/ 忽略规则

--- src/test/kotlin/com/github/xausky/opencodewebui/MyPluginTest.kt
+++ src/test/kotlin/com/github/xausky/opencodewebui/MyPluginTest.kt
@@ -1,14 +1,10 @@
 package com.github.xausky.opencodewebui
 
 import com.intellij.ide.highlighter.XmlFileType
-import com.intellij.openapi.components.service
 import com.intellij.psi.xml.XmlFile
-import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
 
-
-@TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
 
     fun testXMLFile() {
@@ -24,12 +20,4 @@ class MyPluginTest : BasePlatformTestCase() {
             assertEquals("bar", it.value.text)
         }
     }
-
-    fun testRename() {
-        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
-    }
-
-
-
-    override fun getTestDataPath() = "src/test/testData/rename"
 }
